acec39288ef4e72139b8af582cb3c492c3e0ff96
clemsonrpg <loureiro.ravi@gmail.com>
2026-02-23 19:28:25 -0300

    third commit

--- apps/servicos/models.py
+++ apps/servicos/models.py
@@ -3,7 +3,7 @@ from datetime import date
 # Create your models here.
 class Servico(models.Model):
     nome_servico = models.CharField(max_length=100, choices=[
-        ("CEFIR", "Cefir"), ("Contrato de comodato", "Contrato de comodato"), ("Declaração de posse", "Declaração de posse"), ("CCIR", "CCIR"), ("ITR", "ITR")])
+        ("CEFIR", "Cefir"), ("Contrato de comodato", "Contrato de comodato"), ("Declaração de posse", "Declaração de posse"), ("CCIR", "CCIR"), ("ITR", "ITR"), ("Compra e venda", "Compra e venda"), ("Doação de imóvel", "Doação de imóvel")])
     descricao = models.TextField(blank=True, null=True)
     data_servico = models.DateField(default=date.today)
     data_criacao = models.DateTimeField(auto_now_add=True)
